a733a4d1c5aaf359e7bed8fc8c2664263d86b113
theijhay <olawaleisaacjohn@gmail.com>
2024-05-22 14:09:31 +0100

    a class LRUCache that inherits from BaseCaching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A class LRUCache that inherits from BaseCaching"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Create a class LRUCache that inherits from BaseCaching"""
+
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                # Remove the existing key from order to update its position
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Evict the least recently used item (LRU)
+                lru_key = self.order.pop(0)
+                del self.cache_data[lru_key]
+                print(f"DISCARD: {lru_key}")
+
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data:
+            # Move the accessed key to the end to mark it as recently used
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
