2935bc36aa99b90d677e5d18c88f288be514e18e
theijhay <olawaleisaacjohn@gmail.com>
2024-05-24 21:31:49 +0100

    Implement a get_hyper_index

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -4,12 +4,12 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-import math
 from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -17,7 +17,8 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -27,7 +28,8 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0"""
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
@@ -38,40 +40,27 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        Retrieves a dictionary containing hypermedia metadata
-        for the requested index and page size.
-
-        Args:
-            index (int, optional): The current start index of the page.
-            Defaults to None.
-            page_size (int, optional): The number of items per page.
-            Defaults to 10.
-
-        Returns:
-            Dict: A dictionary containing hypermedia metadata
-            for the requested index and page size.
+        Returns a dictionary with key-value pairs
+         (index, next_index, page_size, data)
+        :param index:
+        :param page_size:
+        :return:
         """
-        assert index is None or (isinstance(index, int) and index >= 0)
-
-        indexed_dataset = self.indexed_dataset()
-        keys = sorted(indexed_dataset.keys())
-
-        if index is None:
-            index = keys[0]
-
+        assert type(index) == int
+        assert type(page_size) == int
+        csv = self.indexed_dataset()
+        csv_size = len(csv)
+        assert 0 <= index < csv_size
         data = []
-        next_index = None
-
-        for i in range(index, len(keys)):
-            if len(data) < page_size:
-                data.append(indexed_dataset[keys[i]])
-            else:
-                next_index = keys[i]
-                break
-
+        _next = index
+        for _ in range(page_size):
+            while not csv.get(_next):
+                _next += 1
+            data.append(csv.get(_next))
+            _next += 1
         return {
             "index": index,
-            "next_index": next_index,
-            "page_size": page_size,
             "data": data,
+            "page_size": page_size,
+            "next_index": _next
         }
