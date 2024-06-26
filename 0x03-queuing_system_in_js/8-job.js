1fdb82aea0f4691415ea9a384706014a893b375e
theijhay <olawaleisaacjohn@gmail.com>
2024-06-26 13:06:17 +0100

    Writing the test for job creation

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,34 +1,22 @@
-import kue from 'kue';
-
-/**
- * Create push notification jobs in the provided Kue queue.
- * @param {Array} jobs - An array of job objects to be added to the queue.
- * @param {Object} queue - The Kue queue object.
- * @throws Will throw an error if jobs is not an array.
- */
 function createPushNotificationsJobs(jobs, queue) {
     if (!Array.isArray(jobs)) {
-        throw new Error('Jobs is not an array');
+      throw Error('Jobs is not an array');
     }
-
-    jobs.forEach((jobData) => {
-        const job = queue.create('push_notification_code_3', jobData)
-            .save((err) => {
-                if (err) {
-                    console.log(`Failed to create job: ${err}`);
-                    return;
-                }
-                console.log(`Notification job created: ${job.id}`);
-            });
-
-        job.on('complete', () => {
-            console.log(`Notification job ${job.id} completed`);
-        }).on('failed', (err) => {
-            console.log(`Notification job ${job.id} failed: ${err}`);
-        }).on('progress', (progress) => {
-            console.log(`Notification job ${job.id} ${progress}% complete`);
-        });
+  
+    jobs.forEach((myJob) => {
+      let job = queue.create('push_notification_code_3', myJob);
+  
+      job.on('complete', function() {
+        console.log(`Notification job ${job.id} completed`);
+      }).on('failed', function(error) {
+        console.log(`Notification job ${job.id} failed: ${error}`);
+      }).on('progress', function(progress, data) {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+      job.save((error) => {
+        if (!error) console.log(`Notification job created: ${job.id}`);
+      });
     });
-}
-
-export default createPushNotificationsJobs;
+  }
+  
+  module.exports = createPushNotificationsJobs;
